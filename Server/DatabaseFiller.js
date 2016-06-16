fd98879b3c5f52a6a998c8a8653e5259268bebff
Mathieu Fontenille <fontenille.mathieu@gmail.com>
2016-06-16 16:48:08 +0200

    DatabaseFiller v0.5.1 : new data model (need to fix live data stream)

--- Server/DatabaseFiller.js
+++ Server/DatabaseFiller.js
@@ -7,7 +7,6 @@ var basicPassword = "laboheme55";
 
 function sendLastDatas(socket, items){
     socket.emit('lastDatas', {datas: items});
-    console.log("Données envoyées");
 }
 function sendLogForClient(socket, log){
     socket.emit('logForApp', {err:log});
@@ -21,6 +20,16 @@ function passwordCheck(data, socket){
     var check = data.password == basicPassword;
     if (!check) sendLogForClient(socket, "Erreur : mot de passe non valide");
     return check;
+}
+function prepareAndSendLastDatasForNewConnection(socket, collectionToSend){
+    collectionToSend.find().sort({"date":-1}).limit(7).toArray().then((items) => {
+        for(var item in items){
+            if(!items.hasOwnProperty(item)){continue;}
+            items[item].type = collectionToSend.collectionName.toString();
+        }
+        sendLastDatas(socket,items);
+    });
+    console.log("Données récentes envoyées");
 }
 
 mongo.connect(serverAddress, (err, db) => {
@@ -39,55 +48,69 @@ mongo.connect(serverAddress, (err, db) => {
     
     var io = require('socket.io').listen(webServer);
     io.sockets.on('connection', (socket) => {
-        var doc = db.collection('ititourContent');
+        var doc = db.collection('ititourContent'),
+            departementColl = db.collection('Departement'),
+            villeColl = db.collection('Ville'),
+            siteColl = db.collection('Site'),
+            colls = [departementColl,villeColl,siteColl];
 
         console.log("Connexion depuis l'adresse : " + socket.request.connection.remoteAddress);
 
-        doc.find().sort({"date":-1}).limit(20).toArray().then(
-            (items) => {
-                sendLastDatas(socket,items);
-            }
-        );
+        for(var i=0, x = colls.length; i < x; i++) {
+            prepareAndSendLastDatasForNewConnection(socket, colls[i]);
+        }
+
         socket.on('datasToPush', (data) => {
             if (passwordCheck(data, socket)) {
                 if (data.note > 0 && data.note <=5) {
-                    var date = new Date();
-                    var dataToPush = {
-                        type: data.type, name: data.name,
-                        note: data.note, keywords: data.keywords,
+                    var date = new Date(),
+                        dataToPush = {
+                        name: data.name, note: data.note,
                         desc: data.desc, date
-                    };
-
-                    db.collection('ititourContent', (err, col) => {
-                        console.log(`Réception de données, tentative d'insertion dans ${col}`);
-                        if (data.type == "Departement") {
+                    },
+                        type = data.type,
+                        lastCollModified;
+                    
+                    if(type == "Departement") {
+                        db.collection(type, (err, col) => {
                             dataToPush.villes = data.villes;
                             col.insert(dataToPush);
-                        } else if (data.type == "Ville") {
+                            lastCollModified = type;
+                        });
+                    } else if(type == "Ville") {
+                        db.collection(type, (err, col) => {
+                            dataToPush.keywords = data.keywords;
                             dataToPush.departement = data.departement;
                             dataToPush.sites = data.sites;
                             col.insert(dataToPush);
-                        } else if (data.type == "Site") {
+                            lastCollModified = type;
+                        });
+                    } else if (type == "Site") {
+                        db.collection(type, (err, col) => {
+                            dataToPush.keywords = data.keywords;
                             dataToPush.linkedVilles = data.linkedVilles;
                             col.insert(dataToPush);
-                        }
-                        sendLastDatas(socket, {datas:dataToPush});
-                    });
-                }
-                else
+                            lastCollModified = type;
+                        });
+                    }
+                    
+                    if (lastCollModified) {
+                        var newData = db.collection(lastCollModified).find({"date": -1}).limit(1);
+                        newData.type = data.type.toString();
+                        sendLastDatas(newData);
+                        console.log(`J'ai envoyé un(e) ${type} qui contenait ${newData.name}`);
+                    }
+                } else
                     sendLogForClient(socket, "Données non valides (vérifiez l'orthographe et les champs saisits)");
             }
         });
 
         socket.on('askForDeletion', (data) => {
-            if (passwordCheck(data, socket)) {
-                var idToDelete = (data.id).toString(); //This is the document's id I want to delete, received from the client interface
+            if (passwordCheck(data, socket) && data.type != undefined) {
+                var idToDelete = (data.id).toString(),
+                    collectionDesired = (data.type).toString();
                 console.log(idToDelete);
-                /* the id console.logged is valid (example: 575807172154b7a019ebf6db)
-                 * I can see the same id on my database but ...
-                 */
-
-                doc.deleteOne({"_id": mongodb.ObjectID(idToDelete)}, (err, results) => { //... here is the problem : no deletion
+                db.collection(collectionDesired).deleteOne({"_id": mongodb.ObjectID(idToDelete)}, (err, results) => { //... here is the problem : no deletion
                     console.log(`Request success : ${results.result.ok}, documents deleted : ${results.result.n}`);
                     //Request success : 1, documents deleted : 0, so the document is still on my database
                     if (results.result && results.result.n > 0) {
@@ -99,6 +122,7 @@ mongo.connect(serverAddress, (err, db) => {
                 });
             }
         });
+        
         /*
         if (socket.request.connection.remoteAddress == "::ffff:192.168.0.27")
             setTimeout(function () {
