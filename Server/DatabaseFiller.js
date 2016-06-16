71a59161ab617e813c8c130619e34024c54cdb01
Mathieu Fontenille <fontenille.mathieu@gmail.com>
2016-06-16 09:44:26 +0200

    DatabaseFiller v0.5.0 : deletion is OK !

--- Server/DatabaseFiller.js
+++ Server/DatabaseFiller.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var mongo = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
 var serverAddress = "mongodb://192.168.0.37:27017/Ititour";
 var basicPassword = "laboheme55";
 
@@ -19,17 +20,16 @@ function modifiedDataConfirmation(socket, confirmationType, dataId){
 function passwordCheck(data, socket){
     var check = data.password == basicPassword;
     if (!check) sendLogForClient(socket, "Erreur : mot de passe non valide");
-    return data.password == basicPassword;
+    return check;
 }
 
-mongo.connect(serverAddress, function (err, db) {
+mongo.connect(serverAddress, (err, db) => {
     if (err) console.log("Impossible de se connecter ", err);
     else console.log("Connection to database:  OK");
 
     //On crée le serveur web après avoir réussit à se connecter à la base de données
-    var webServer = http.createServer(function(request,response){
-        fs.readFile('../Views/DatabaseFillerPageView.html', 'utf-8', function(err, data)
-        {
+    var webServer = http.createServer( (request,response) => {
+        fs.readFile('../Views/DatabaseFillerPageView.html', 'utf-8', (err, data) => {
             if (err) console.log(err);
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(data);
@@ -38,17 +38,17 @@ mongo.connect(serverAddress, function (err, db) {
     }).listen(8080);
     
     var io = require('socket.io').listen(webServer);
-    io.sockets.on('connection', function(socket){
+    io.sockets.on('connection', (socket) => {
         var doc = db.collection('ititourContent');
 
         console.log("Connexion depuis l'adresse : " + socket.request.connection.remoteAddress);
 
         doc.find().sort({"date":-1}).limit(20).toArray().then(
-            function (items) {
+            (items) => {
                 sendLastDatas(socket,items);
             }
         );
-        socket.on('datasToPush', function (data) {
+        socket.on('datasToPush', (data) => {
             if (passwordCheck(data, socket)) {
                 if (data.note > 0 && data.note <=5) {
                     var date = new Date();
@@ -58,18 +58,16 @@ mongo.connect(serverAddress, function (err, db) {
                         desc: data.desc, date
                     };
 
-                    db.collection('ititourContent', function (err, col) {
+                    db.collection('ititourContent', (err, col) => {
                         console.log(`Réception de données, tentative d'insertion dans ${col}`);
                         if (data.type == "Departement") {
                             dataToPush.villes = data.villes;
                             col.insert(dataToPush);
-                        }
-                        else if (data.type == "Ville") {
+                        } else if (data.type == "Ville") {
                             dataToPush.departement = data.departement;
                             dataToPush.sites = data.sites;
                             col.insert(dataToPush);
-                        }
-                        else if (data.type == "Site") {
+                        } else if (data.type == "Site") {
                             dataToPush.linkedVilles = data.linkedVilles;
                             col.insert(dataToPush);
                         }
@@ -81,17 +79,21 @@ mongo.connect(serverAddress, function (err, db) {
             }
         });
 
-        socket.on('askForDeletion', function(data){
+        socket.on('askForDeletion', (data) => {
             if (passwordCheck(data, socket)) {
-                var idToDelete = data.id;
+                var idToDelete = (data.id).toString(); //This is the document's id I want to delete, received from the client interface
                 console.log(idToDelete);
+                /* the id console.logged is valid (example: 575807172154b7a019ebf6db)
+                 * I can see the same id on my database but ...
+                 */
 
-                doc.findOneAndDelete({"_id":idToDelete}, function (err, results) {
-                    console.log(results);
+                doc.deleteOne({"_id": mongodb.ObjectID(idToDelete)}, (err, results) => { //... here is the problem : no deletion
+                    console.log(`Request success : ${results.result.ok}, documents deleted : ${results.result.n}`);
+                    //Request success : 1, documents deleted : 0, so the document is still on my database
                     if (results.result && results.result.n > 0) {
                         modifiedDataConfirmation(socket, "deletion", idToDelete);
                     } else {
-                        sendLogForClient(socket, "Aucune correspondance trouvée");
+                        sendLogForClient(socket, "No matches found");
                         console.log(err);
                     }
                 });
