c69d9d2471c3a93e77ba8afea9b80752a9356e76
Mathieu Fontenille <fontenille.mathieu@gmail.com>
2016-06-23 14:44:11 +0200

    Starting API

--- Server/API.js
+++ Server/API.js
@@ -1,22 +1,14 @@
-var app = require('express');
+var express = require('express'),
+    app = express(),
+    dataa = require('./itiAlternative.json'),
+    http = require('http').createServer(app).listen(8080);
 
-var data = require('./Iti.json');
-var dataa = require('./itiAlternative.json');
+app.use(express.static('../Views/Assets'));
 
-/*
-for (var i=0, x = data.days.length; i < x; i++){
-    console.log(data.days[i]);
-    for (var z = 0, w = data.days[i].length; z < w ; z++){
-        var tab = data.days[i][z];
-        console.log(`Première étape ! Rendez vous au ${tab[0]} en suite ${tab[1]}`)
-    }
-}
-*/
-
-for(var i=1, x = dataa.daysNumber; i <= x; i++ ){
-    var day = dataa.days["day" + i.toString()];
-    for(var step in day){
-        var stepObj = day[step];
-        console.log(`Pour commencer ${stepObj.desc} pour rejoindre le ${stepObj.site}`);
-    }
-}
+app.get('/api',function (request, response) {
+   response.json(dataa);
+});
+app.get('/',function (request, response) {
+   response.redirect('/api');
+});
+app.listen(http);
